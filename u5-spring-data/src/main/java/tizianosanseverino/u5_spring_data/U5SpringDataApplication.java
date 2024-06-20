f3783971996c53927e381f34e9daec4b9000b101
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-06-20 15:16:21 +0200

    add project

---
+++ u5-spring-data/src/main/java/tizianosanseverino/u5_spring_data/U5SpringDataApplication.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.u5_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5SpringDataApplication.class, args);
+	}
+
+}
---
+++ u5-spring-data/src/test/java/tizianosanseverino/u5_spring_data/U5SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.u5_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
